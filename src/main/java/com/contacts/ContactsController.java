10c53a012a95528f7ed7be23021875a7cabe00bd
Benoit VALLON <benoitvallon@gmail.com>
2016-04-28 00:29:35 -0400

    Add initial files

---
+++ src/main/java/com/contacts/ContactsController.java
@@ -0,0 +1,17 @@
+package com.contacts;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class ContactsController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/com/contacts/SpringContactsApplication.java
@@ -0,0 +1,12 @@
+package com.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringContactsApplication.class, args);
+    }
+}
