a207312eba235b8bbe62f3e2fa988b3c59d9fcd7
Benoit VALLON <benoitvallon@gmail.com>
2016-04-30 14:05:03 -0400

    Add a home page

--- src/main/java/contacts/ContactController.java
+++ src/main/java/contacts/ContactController.java
@@ -2,14 +2,14 @@ package contacts;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ContactController {
+    @RequestMapping(method= RequestMethod.GET)
+    public String home(Map<String, Object> model) {
+        return "home";
+    }
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
     }
 }
