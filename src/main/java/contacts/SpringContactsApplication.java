1caf998bc04e2717110f64e5c4470b7a1d44c700
Benoit VALLON <benoitvallon@gmail.com>
2016-04-30 14:00:14 -0400

    Fix typos

--- src/main/java/contacts/SpringContactsApplication.java
+++ src/main/java/contacts/SpringContactsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringContactsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringContactsApplication.class, args);
     }
