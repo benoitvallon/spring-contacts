c5e7fe1f23d2d25e09b23115088418aa65490174
Benoit VALLON <benoitvallon@gmail.com>
2016-04-30 09:45:56 -0400

    Remove @EnableAutoConfiguration in controller

--- src/main/java/contacts/ContactsController.java
+++ src/main/java/contacts/ContactsController.java
@@ -1,12 +1,10 @@
 package contacts;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
 public class ContactsController {
 
     @RequestMapping("/")
